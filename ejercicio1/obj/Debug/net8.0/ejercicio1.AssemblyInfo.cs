ffae7d292cd16d0a14d511d69f53efa97a105215
Lalodino <iebs4587@gmail.com>
2025-03-11 21:35:09 -0600

    version 2

--- ejercicio1/obj/Debug/net8.0/ejercicio1.AssemblyInfo.cs
+++ ejercicio1/obj/Debug/net8.0/ejercicio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+922d5441ef84803bc14fff16d19b2821366af6f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ejercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
